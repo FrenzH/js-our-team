e13be4627c533d033ac32320972bb364e9cc4ae2
FrenzH <franciattom99@gmail.com>
2022-09-06 14:50:01 +0100

    let-object-dataCard

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log('JS OK!')
+const dataCard = [
+
+    { name : 'Wayne Barnett',
+      position : 'Founder & CEO',
+      img : 'wayne-barnett-founder-ceo.jpg'},
+      
+    { name : 'Angela Caroll',
+      position : 'Chief Editor',
+      img : 'angela-caroll-chief-editor.jpg'},
+
+    { name : 'Walter Gordon',
+      position : 'Office Manager',
+      img : 'walter-gordon-office-manager.jpg'},
+
+    { name : 'Angela Lopez',
+      position : 'Social Media Manager',
+      img : 'angela-lopez-social-media-manager.jpg'},
+
+    { name : 'Scott Estrada',
+      position : 'Developer',
+      img : 'scott-estrada-developer.jpg'},
+
+    {  name : 'Barbara Ramos',
+       position : 'Graphic Designer',
+       img : 'barbara-ramos-graphic-designer.jpg'}
+];
